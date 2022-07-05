8fa64b1c9d4acbe1da3c3ad74d6ecf29f4e0f7ba
Florjan96 <florianshiqerukaj@yahoo.com>
2022-07-05 23:37:20 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+const baseUrl = 'https://api.openbrewerydb.org/breweries'
+const selectStateForm = document.querySelector("#select-state-form")
+
+let state = {
+   selectedState: '',
+    breweries: []
+  }de here
